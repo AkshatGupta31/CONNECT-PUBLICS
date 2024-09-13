cb5ba876e15c3e06e40670db91ab8b068195f897
Akshat <akshatguptagw@gmail.com>
2024-09-13 23:12:12 +0530

    done for the day

--- HomeJs.js
+++ HomeJs.js
@@ -1,10 +1,1 @@
-const navbar = document.querySelector("#nav");
-let top = navbar.offsetTop;
-function stickynavbar() {
-  if (window.scrollY >= top) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-window.addEventListener("scroll", stickynavbar);
+
